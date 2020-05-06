2bfd8245ad4275e54386fb5bdaed5be8a65248a1
Somya Bansal <sobansal@deloitte.com>
2020-05-06 18:37:19 +0530

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-
-## Somya Bansal
-
-Hey! This is the source code for my [portfolio website](https://somya-bansal.github.io/), built using [create-react-app](https://github.com/facebook/create-react-app)
-
---
+++ precache-manifest.4cfcf5fa3df9250880d8700f702110b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ec85cc6009c6203467936c3fd4ac8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49dc01edc319436bf959",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "bd25bf603745ea560b61",
+    "url": "/static/js/2.c71f1797.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c71f1797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49dc01edc319436bf959",
+    "url": "/static/js/main.f3065a34.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
